c4a447dbb2a9e033b2857f25d75400969cf93949
Brahim <brahim.elkarmouchi@gmail.com>
2021-01-25 16:55:13 +0100

    category title

--- app/src/main/java/fr/isen/elkarmouchi/androiderestauran/CategoryActivity.kt
+++ app/src/main/java/fr/isen/elkarmouchi/androiderestauran/CategoryActivity.kt
@@ -2,10 +2,46 @@ package fr.isen.elkarmouchi.androiderestauran
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import fr.isen.elkarmouchi.androiderestauran.databinding.ActivityCategoryBinding
 
+
+enum class ItemType {
+    STARTER, MAIN, DESSERT
+}
 class CategoryActivity : AppCompatActivity() {
+    private lateinit var bindind: ActivityCategoryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_category)
+        bindind = ActivityCategoryBinding.inflate(layoutInflater)
+        setContentView(bindind.root)
+
+        val selectedItem = intent.getSerializableExtra(HomeActivity.CATEGORY_NAME) as? ItemType
+        bindind.categoryTitle.text = getCategoryTitle(selectedItem)
+
+        Log.d("lifecycle", "onCreate")
+    }
+    private fun getCategoryTitle(item: ItemType?): String {
+        return when(item) {
+            ItemType.STARTER -> getString(R.string.starter)
+            ItemType.MAIN -> getString(R.string.main)
+            ItemType.DESSERT -> getString(R.string.deserts)
+            else -> ""
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d("lifecycle", "onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("lifecycle", "onRestart")
+    }
+
+    override fun onDestroy() {
+        Log.d("lifecycle", "onDestroy")
+        super.onDestroy()
     }
 }
--- app/src/main/java/fr/isen/elkarmouchi/androiderestauran/HomeActivity.kt
+++ app/src/main/java/fr/isen/elkarmouchi/androiderestauran/HomeActivity.kt
@@ -2,10 +2,8 @@ package fr.isen.elkarmouchi.androiderestauran
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.util.Log
-import android.widget.Toast
+import android.content.Intent
+import androidx.annotation.StringRes
 import fr.isen.elkarmouchi.androiderestauran.databinding.ActivityHomeBinding
 
 
@@ -13,26 +11,50 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
       binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
         binding.button2.setOnClickListener {
-
-            Toast.makeText(this, getString(R.string.starter),Toast.LENGTH_SHORT).show()
+            statCategoryActivity(ItemType.STARTER)
+           // redirectToCategory("Entrées")
+            //Toast.makeText(this, getString(R.string.starter),Toast.LENGTH_SHORT).show()
 
         }
         binding.button3.setOnClickListener {
+             statCategoryActivity(ItemType.MAIN)
+           // redirectToCategory("Plats")
 
-            Toast.makeText(this, getString(R.string.main),Toast.LENGTH_SHORT).show()
+           //Toast.makeText(this, getString(R.string.main),Toast.LENGTH_SHORT).show()
 
         }
         binding.button4.setOnClickListener {
 
-            Toast.makeText(this, getString(R.string.deserts),Toast.LENGTH_SHORT).show()
+            statCategoryActivity(ItemType.DESSERT)
+          //  redirectToCategory("Desserts")
+           // Toast.makeText(this, getString(R.string.deserts),Toast.LENGTH_SHORT).show()
 
         }
-          
+
+
+
+    }
+    /*private fun redirectToCategory(@StringRes categoryRes: Int) {
+        val intent = Intent(this, CategoryActivity::class.java)
+        intent.putExtra(CATEGORY_NAME, getString(categoryRes))
+        startActivity(intent)
+    }*/
+
+    private fun statCategoryActivity(item: ItemType) {
+        val intent = Intent(this, CategoryActivity::class.java)
+        intent.putExtra(CATEGORY_NAME, item)
+        startActivity(intent)
+    }
+
+
+    companion object {
+        const val CATEGORY_NAME = "CATEGORY_NAME"
     }
 
 }
