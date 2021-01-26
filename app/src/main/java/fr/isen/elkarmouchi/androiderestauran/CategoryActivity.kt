da96c0466cda921aec74914a370167da22457c8b
Brahim <brahim.elkarmouchi@gmail.com>
2021-01-26 13:07:21 +0100

    Volley and Gson Request Menu

--- app/src/main/java/fr/isen/elkarmouchi/androiderestauran/CategoryActivity.kt
+++ app/src/main/java/fr/isen/elkarmouchi/androiderestauran/CategoryActivity.kt
@@ -5,8 +5,18 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.GsonBuilder
 import fr.isen.elkarmouchi.androiderestauran.Category.CategoryAdapter
 import fr.isen.elkarmouchi.androiderestauran.databinding.ActivityCategoryBinding
+import fr.isen.elkarmouchi.androiderestauran.network.MenuResult
+import fr.isen.elkarmouchi.androiderestauran.network.NetworkConstant
+import kotlinx.android.synthetic.main.activity_category.*
+import org.json.JSONObject
 
 
 enum class ItemType {
@@ -24,9 +34,12 @@ class CategoryActivity : AppCompatActivity() {
         val selectedItem = intent.getSerializableExtra(HomeActivity.CATEGORY_NAME) as? ItemType
         bindind.categoryTitle.text = getCategoryTitle(selectedItem)
         loadList()
+        makeRequest()
         Log.d("lifecycle", "onCreate")
+
+
     }
-    
+
     private fun loadList() {
         var entries = listOf<String>("salade","boeuf","glace")
         val adapter =
@@ -35,6 +48,35 @@ class CategoryActivity : AppCompatActivity() {
             )
         bindind.recyclerView.layoutManager = LinearLayoutManager(this)
         bindind.recyclerView.adapter = adapter
+    }
+
+    private fun makeRequest(){
+        val queue = Volley.newRequestQueue(this)
+        val jsondata= JSONObject()
+        jsondata.put("id_shop", 1)
+       // val url = "http://test.api.catering.bluecodegames.com/menu"
+        var url = NetworkConstant.BASE_URL + NetworkConstant.PATH_MENU
+
+// Request a string response from the provided URL.
+        val stringRequest= JsonObjectRequest(Request.Method.POST,
+            url,
+            jsondata,
+            { response ->
+                //Log.d("request",response.toString(2))
+                val menu = GsonBuilder().create().fromJson(response.toString(), MenuResult::class.java)
+                menu.data.forEach{
+                    Log.d("Request", it.name)
+                }
+            },
+            {error ->
+                Log.d("Request", error.localizedMessage)
+            }
+            )
+
+
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest)
     }
     
     private fun getCategoryTitle(item: ItemType?): String {
